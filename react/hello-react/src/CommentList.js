0f880b3f9349089b2aaeceb219ac852b111ecaa3
2941187216 <2941187216@qq.com>
2020-05-01 19:59:26 +0800

    react

---
+++ react/hello-react/src/CommentList.js
@@ -0,0 +1,23 @@
+import React, { useLayoutEffect } from 'react';
+
+
+function CommentList () {
+    return (
+        <ul>
+            <li>
+                用户名：111
+            </li>
+            <li>
+                用户名：111
+            </li>
+            <li>
+                用户名：111
+            </li>
+            <li>
+                用户名：111
+            </li>
+        </ul>
+    )
+}
+
+export default CommentList;
