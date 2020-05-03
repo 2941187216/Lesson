8c4559da6b7f787e192adde2f31bb6d8d2806b9f
2941187216 <2941187216@qq.com>
2020-05-03 19:47:08 +0800

    读书笔记

---
+++ node.pi/chapter6/1.js
@@ -0,0 +1,9 @@
+// console.log("0123456789".length);
+
+// console.log("零一二三四五六七八九".length);
+
+// console.log("\u00bd".length);
+
+var str = "深入浅出node.js";
+var buf = new Buffer(str, 'utf-8');
+console.log(buf);
