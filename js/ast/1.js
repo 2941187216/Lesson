727bb8bd25c549b0f6839fff1a59d0fbc388f1ae
2941187216 <2941187216@qq.com>
2020-05-10 16:21:06 +0800

    ast

---
+++ js/ast/1.js
@@ -0,0 +1,38 @@
+{/* <div id="root" class="wrap">
+        <span class="demo"> 
+            This is a span.
+        </span>
+        <p>DOM</p>
+</div> */}
+
+let dom = {
+    tag: 'div',
+    attrs: {
+        id:'root',
+        className:'wrap'
+    },
+    children:[
+        {
+            tag:'span',
+            attrs:{
+                className:'demo'
+            },
+            children:[
+                {
+                   tag: undefined,
+                   text:'This is a span.' 
+                }
+            ]
+        },
+        {
+            tag:'p',
+            children:[
+                {
+                    tag:undefined,
+                    text:'DOM'
+                }
+            ]
+        }
+
+    ]
+}
