5fd9f97562f5cd103eb70cc00be5cd053c6af3cf
2941187216 <2941187216@qq.com>
2020-05-13 21:35:58 +0800

    comment

--- react/react-comment-app/src/CommentList.js
+++ react/react-comment-app/src/CommentList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
+import Comment from './Comment';
 class ComponentList extends Component {
   render() {
+    console.log(this.props, '++++');
+    let { comments } = this.props; //  在下面不用this.props.comments 
+    
     return (
       <div>
-        ComponentList
+        { comments.map((comment, i) => <Comment comment={comment} key={i}/>) }
       </div>
-      
     )
   }
 }
