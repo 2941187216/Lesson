a92e67dc602e0386317c6a2cce0d82f9f4173464
2941187216 <2941187216@qq.com>
2020-05-27 21:09:35 +0800

    do-comment-redux

---
+++ react/do-comment-redux/src/reducers/comments.js
@@ -0,0 +1,17 @@
+// reducer 就是函数
+// 初始状态
+let comments = [
+    {
+      "id": 1,
+      "username": "刘达",
+      "content": "帅哥， "
+    }
+  ];
+
+  export default function(state = {
+    comments: comments
+  }) {
+
+    // 初始状态
+    return state
+  }
