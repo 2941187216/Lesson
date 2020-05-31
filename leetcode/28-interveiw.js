ba719c3763b5fe220f3322ea10ba932ea9041179
2941187216 <2941187216@qq.com>
2020-05-31 09:05:57 +0800

    对称二叉树

---
+++ leetcode/28-interveiw.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(!root) return true;
+    function helper(left, right) {
+        if(left === null && right === null) return true;
+        if(!left || !right) return false;
+        if(left.val != right.val ) return false;
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+    return helper(root.left, root.right);
+};
