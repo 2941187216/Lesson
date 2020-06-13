722cb9144160ed7b6d63d7324176187c7de62a52
2941187216 <2941187216@qq.com>
2020-06-13 22:01:35 +0800

    24-interview.js

---
+++ leetcode/24-interview.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    // 初始化前驱结点为 null
+    let pre = null;
+    // 初始化目标结点为头结点
+    let cur = head;
+    // 只要目标结点不为 null，遍历就得继续
+    while (cur !== null) {
+        // 记录一下 next 结点
+        let next = cur.next;
+        // 反转指针
+        cur.next = pre;
+        // pre 往前走一步
+        pre = cur;
+        // cur往前走一步
+        cur = next;
+    }
+    // 反转结束后，pre 就会变成新链表的头结点
+    return pre
+};
