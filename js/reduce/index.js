d684c00817de04e518b5509fb27e27774705fa4b
2941187216 <2941187216@qq.com>
2020-07-02 16:31:17 +0800

    reduce

---
+++ js/reduce/index.js
@@ -0,0 +1,9 @@
+var arr = [1, 2, 3, 4];
+// reduce total
+// 1. 可读性 代码风格
+var sum = arr.reduce(function (pre, cur, index, arr) {
+    console.log(pre, cur, index)
+    return pre + cur;
+}, 0);
+
+console.log(sum)
