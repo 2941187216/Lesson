4085825fe0df490d5618899e824a3517e9c3fddd
2941187216 <2941187216@qq.com>
2020-07-19 23:51:24 +0800

    chapter1

--- books/js_you_donot_know/chapter1/5.js
+++ books/js_you_donot_know/chapter1/5.js
@@ -1,8 +1,20 @@
 
-"use strict"
-function init(a) {
-    b = a + 3;// LHS 声明在全局
-}
+// "use strict"
+// function init(a) {
+//     b = a + 3;// LHS 声明在全局
+// }
 
-init(2);
-console.log(b);
+// init(2);
+// console.log(b);
+
+function fn(a) {
+    console.log(a);
+    var a = 123;
+    console.log(a);
+    function a() {}
+    console.log(a);
+    var b = function () {};
+    console.log(b);
+    function d() {}
+}
+fn(1);
